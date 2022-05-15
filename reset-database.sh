f2acb69ed82385914e09f1f8e4671cdabd4acd44
Jesper Beisner <jesper.beisner+github@gmail.com>
2022-05-15 20:56:14 +0200

    clean up reset-database script

--- reset-database.sh
+++ reset-database.sh
@@ -1,10 +1,10 @@
-# Online for the first local development sessions :)
+# Only for local development sessions :)
 
-php bin/console doctrine:database:drop --force
-php bin/console doctrine:database:create
+php bin/console doctrine:database:drop --force --quiet
+php bin/console doctrine:database:create --quiet
 
-rm migrations/*
-php bin/console doctrine:migrations:diff
+php bin/console doctrine:migrations:migrate --no-interaction --quiet
+php bin/console doctrine:migrations:diff --allow-empty-diff --no-interaction --quiet
+php bin/console doctrine:migrations:migrate --no-interaction --quiet
 
-php bin/console doctrine:migrations:migrate --no-interaction
-php bin/console doctrine:fixtures:load --no-interaction
+php bin/console doctrine:fixtures:load --no-interaction --quiet
